eaa4a5295a81cf9d76ca7210080bc14ce42b6ff8
boseon <qhtjs3223@gmail.com>
2024-04-16 13:35:48 +0900

    240416 저장

---
+++ config/database/db_config.js
@@ -0,0 +1,7 @@
+const dbconfig = {
+    user : "kim",
+    password : "1234",
+    connectString : "localhost:1521/xe"
+}
+
+module.exports = dbConfig;
--- src/database/dao.js
+++ src/database/dao.js
@@ -1,1 +1,7 @@
-// 무시
+const oracledb = require("oracledb");
+const dbConfig = require("../../config/database/db_config")
+oracledb.autoCommit = true;
+oracledb.outFormat = oracledb.OBJECT;
+
+
+module.exports = oracledb.getConnection(dbConfig);
