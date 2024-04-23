1a1802bfcb6a89f768cad5efe0c212acf6127763
gayoung <gyy94@naver.com>
2024-04-23 17:56:31 +0900

    test05

--- panda.js
+++ panda.js
@@ -1,13 +1,21 @@
 const express = require("express");
 
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+
 const app = express();
 
 app.use("/static", express.static("./resources"));
+
 //app.use(express.static("resource"))
 
+/*
 const bodyParser=require("body-parser")
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+*/
 
 app.get("/", function(req, res){
     res.render("upload.ejs")
@@ -21,3 +29,18 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.listen(3000, () => console.log("3000 서버 구동!"));
+
+app.use( bodyParser.urlencoded() );
+app.use( bodyParser.json() );
+app.use( cookieParser() );
+
+//const router = require("./router")(app);
+//app.use("/", router);
+
+
+
+app.set("views","./src/views");
+app.set("view engine","ejs");
+
+app.listen(3000,()=>{ console.log("3000 port server"); });
+
--- router.js
+++ router.js
@@ -1,3 +1,4 @@
+
 module.exports = (app) => {
 
     const infoRouter = require("./src/routers/info/info_router");
@@ -14,3 +15,20 @@ module.exports = (app) => {
     })
     return router;
 }
+
+module.exports = (app)=>{
+  const cookieParser = require("cookie-parser");
+  const paymentRouter = require("./src/routers/payment/payment_routers");
+  const mainRouter = require("./src/routers/main/main_routers");
+  const loginRouter = require("./src/routers/login/login_routers");
+
+  app.use( cookieParser() );
+  app.use("/payment", paymentRouter );
+  app.use("/", mainRouter );
+  app.use("/login", loginRouter );
+
+  const router = require("express").Router();
+
+  return router;
+}
+
