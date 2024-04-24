d1dc5c647bebb0bf87391c7dd64eb779515d0609
G1Coding <32190250@dankook.ac.kr>
2024-04-24 14:16:22 +0900

    form 저장/불러오기 기능 완료

--- panda.js
+++ panda.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use( bodyParser.urlencoded(true) );
 const router = require("./router") (app);
 
 app.use("/", router);
 
+app.use("/resources", express.static("resources")); 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.listen(3000, () => console.log("3000 서버 구동!"));
+
+app.listen(3000,()=>{ console.log("3000 port server"); });
+
--- router.js
+++ router.js
@@ -1,11 +1,16 @@
-module.exports = (app) => {
-    const boardRouter = require("./src/routers/board/board_router");
-    app.use("/board", boardRouter);
+module.exports = (app)=>{
+  const cookieParser = require("cookie-parser");
+  const paymentRouter = require("./src/routers/payment/payment_routers");
+  const mainRouter = require("./src/routers/main/main_routers");
+  const loginRouter = require("./src/routers/login/login_routers");
+  const boardRouter = require("./src/routers/board/board_router");
+
+app.use("/board", boardRouter);
 
     const router = require("express").Router();
     router.get("/", (req, res) => {
-        res.send("초기 페이지");
+        res.render("index");
     })
 
+
     return router;
-}
